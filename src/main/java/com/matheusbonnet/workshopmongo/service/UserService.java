8919bfe6f5499f4cfaf29a683730287a66015e63
Matheus Bonnet <matheusbonnet2019@gmail.com>
2021-06-16 17:20:35 -0300

    Atualização de usuário com PUT

--- src/main/java/com/matheusbonnet/workshopmongo/service/UserService.java
+++ src/main/java/com/matheusbonnet/workshopmongo/service/UserService.java
@@ -2,8 +2,10 @@ package com.matheusbonnet.workshopmongo.service;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.matheusbonnet.workshopmongo.domain.User;
 import com.matheusbonnet.workshopmongo.dto.UserDTO;
 import com.matheusbonnet.workshopmongo.repository.UserRepository;
@@ -33,6 +35,17 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}

119ee16b7088b0f915f40128dc57008e1b5f9a72
Matheus Bonnet <matheusbonnet2019@gmail.com>
2021-06-16 14:10:10 -0300

    Obtendo um usuário por id

--- src/main/java/com/matheusbonnet/workshopmongo/resources/UserResource.java
+++ src/main/java/com/matheusbonnet/workshopmongo/resources/UserResource.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,11 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> dto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@RequestMapping(value = "/{id:.*}", method = RequestMethod.GET)	
+	public ResponseEntity<UserDTO> findById(@PathVariable("id") final String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/matheusbonnet/workshopmongo/service/UserService.java
+++ src/main/java/com/matheusbonnet/workshopmongo/service/UserService.java
@@ -1,12 +1,12 @@
 package com.matheusbonnet.workshopmongo.service;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.matheusbonnet.workshopmongo.domain.User;
 import com.matheusbonnet.workshopmongo.repository.UserRepository;
+import com.matheusbonnet.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +16,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }

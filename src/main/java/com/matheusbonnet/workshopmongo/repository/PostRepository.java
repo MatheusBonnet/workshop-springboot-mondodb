18bd157a7e6792dd50a97968b1a9f3a68c552d29
Matheus Bonnet <matheusbonnet2019@gmail.com>
2021-06-16 17:56:03 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/matheusbonnet/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.matheusbonnet.workshopmongo.repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusbonnet.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+
+}

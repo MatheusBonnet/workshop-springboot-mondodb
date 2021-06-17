d108f3fafa907edc90a9d03143ece341d04902a0
Matheus Bonnet <matheusbonnet2019@gmail.com>
2021-06-17 15:18:18 -0300

    Consulta simples com @Query

--- src/main/java/com/matheusbonnet/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/matheusbonnet/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.matheusbonnet.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.matheusbonnet.workshopmongo.domain.Post;
@@ -9,6 +10,9 @@ import com.matheusbonnet.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }

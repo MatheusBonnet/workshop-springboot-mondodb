1604336ec8cc7936548cfe06ac91c121efdd7807
Matheus Bonnet <matheusbonnet2019@gmail.com>
2021-06-17 14:23:54 -0300

    Obtendo um post por id

---
+++ src/main/java/com/matheusbonnet/workshopmongo/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.matheusbonnet.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.matheusbonnet.workshopmongo.domain.Post;
+import com.matheusbonnet.workshopmongo.service.PostService;
+
+@RestController
+
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@RequestMapping(value = "/{id:.*}", method = RequestMethod.GET)
+
+	public ResponseEntity<Post> findById(@PathVariable("id") final String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
